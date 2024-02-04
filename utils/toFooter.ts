e8795849d9dc8bfdc31c77c7331cfa43945b27cd
Pawan Kumar <pawan.stardust@gmail.com>
2024-02-04 14:06:58 +0530

    fix build error

--- utils/toFooter.ts
+++ utils/toFooter.ts
@@ -6,7 +6,7 @@ import { getShareAnswerURL, getSolutionsURL, getRepoREADMEUrl } from "./url";
 export const toFooter = function (quiz: Question, locale: SupportedLocale) {
   return (
     "\n\n" +
-    `> ${translate(locale, "link.share-solutions")}${getShareAnswerURL({ question: quiz, locale })}\n` +
+    `> ${translate(locale, "link.share-solutions")}${getShareAnswerURL({ challenge: quiz, locale })}\n` +
     `> ${translate(locale, "link.checkout-solutions")}${getSolutionsURL(quiz.no)}\n` +
     `> ${translate(locale, "link.more-challenges")}${getRepoREADMEUrl(locale)}\n`
   );
