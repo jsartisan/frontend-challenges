c7aa8ff037ced79444596188254c57c48c816dc1
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-03-17 11:06:48 +0530

    remove console

--- components/editor/Preview.tsx
+++ components/editor/Preview.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { memo } from "react";
+import { SandpackPreview, SandpackTests, useSandpack } from "@codesandbox/sandpack-react";
+import { Card } from "../ui/card";
 import { cn } from "@/utils/helpers";
-import { TEMPLATES } from "@/templates";
 import { useEffect, useState } from "react";
 import { SupportedTemplates } from "@/types";
-
-import { Card } from "../ui/card";
-import { Tabs, TabsList, TabsTrigger } from "../ui";
-import { SandpackConsole, SandpackPreview, SandpackTests, useSandpack } from "@codesandbox/sandpack-react";
+import { TEMPLATES } from "@/templates";
+import { memo } from "react";
 
 type PreviewProps = {
   className?: string;
@@ -23,17 +21,12 @@ function Preview(props: PreviewProps) {
   const { activeFile } = sandpack;
   const isTestFile = activeFile.includes(".test.");
   const mode = "mode" in TEMPLATES[template] ? TEMPLATES[template].mode : undefined;
-  const [activeTab, setActiveTab] = useState<"console" | "preview">("preview");
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 500);
   }, []);
-
-  // const { logs } = useSandpackConsole({ resetOnPreviewRestart: true });
-
-  // console.log({ logs });
 
   return (
     <Card className={cn("relative flex h-full w-full flex-col overflow-hidden", className)}>
@@ -44,36 +37,21 @@ function Preview(props: PreviewProps) {
         </div>
       ) : (
         <>
-          <div className="absolute inset-0">
-            <Tabs value={activeTab} className="absolute inset-0" onValueChange={(value) => setActiveTab(value as any)}>
-              <TabsList>
-                <TabsTrigger value="preview">{mode === "tests" ? "Tests" : "Preview"}</TabsTrigger>
-                <TabsTrigger value="console">Console</TabsTrigger>
-              </TabsList>
-              <div className={cn("relative hidden h-[calc(100%-40px)] w-full", activeTab === "preview" && "block")}>
-                <SandpackTests
-                  showWatchButton={false}
-                  showVerboseButton={false}
-                  className={cn(
-                    "!absolute !bottom-0 !left-0 !right-0 !top-0",
-                    isTestFile || mode == "tests" ? "z-10" : "z-0",
-                  )}
-                />
-                <SandpackPreview
-                  showOpenNewtab
-                  showNavigator
-                  showOpenInCodeSandbox={false}
-                  className={cn(
-                    "!absolute !bottom-0 !left-0 !right-0 !top-0",
-                    isTestFile || mode == "tests" ? "z-0" : "z-10",
-                  )}
-                />
-              </div>
-              <div className={cn("relative hidden h-[calc(100%-40px)]", activeTab === "console" && "block")}>
-                <SandpackConsole showHeader />
-              </div>
-            </Tabs>
-          </div>
+          <SandpackTests
+            className={cn(
+              "!absolute !bottom-0 !left-0 !right-0 !top-0",
+              isTestFile || mode == "tests" ? "z-10" : "z-0",
+            )}
+          />
+          <SandpackPreview
+            showOpenNewtab
+            showNavigator
+            showOpenInCodeSandbox={false}
+            className={cn(
+              "!absolute !bottom-0 !left-0 !right-0 !top-0",
+              isTestFile || mode == "tests" ? "z-0" : "z-10",
+            )}
+          />
         </>
       )}
     </Card>
