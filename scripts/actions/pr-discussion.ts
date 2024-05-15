c4ed3e1bc466116f205f66d6935f23bc0e9d74f5
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-05-15 23:11:07 +0530

    update workflow

--- scripts/actions/pr-discussion.ts
+++ scripts/actions/pr-discussion.ts
@@ -4,57 +4,19 @@ import fs from "fs-extra";
 import type { Action } from "../../types";
 import { DEFAULT_LOCALE } from "../../constants";
 
-const Messages = {
-  en: {
-    info: "Info",
-    template: "Template",
-    tests: "Test Cases",
-    issue_reply: "#{0} - Pull Request created.",
-    issue_update_reply: "#{0} - Pull Request updated.",
-    issue_invalid_reply: "Failed to parse the issue, please follow the template.",
-    pr_auto_translate_tips: "Auto translated by Google Translate",
-  },
-  "zh-CN": {
-    info: "基本信息",
-    template: "题目模版",
-    tests: "判题测试",
-    issue_reply: "#{0} - PR 已生成",
-    issue_update_reply: "#{0} - PR 已更新",
-    issue_invalid_reply: "Issue 格式不正确，请按照依照模版修正",
-    pr_auto_translate_tips: "通过谷歌 API 自动翻译",
-  },
-  ja: {
-    info: "基本情報",
-    template: "テンプレート",
-    tests: "テストケース",
-    issue_reply: "#{0} - Pull Request created.",
-    issue_update_reply: "#{0} - Pull Request updated.",
-    issue_invalid_reply: "Failed to parse the issue, please follow the template.",
-    pr_auto_translate_tips: "Auto translated by Google Translate",
-  },
-};
-
 export const getOthers = <A, B>(condition: boolean, a: A, b: B): A | B => (condition ? a : b);
 
 const action: Action = async (github, context, core) => {
-  const payload = context.payload || {};
-  const issue = payload.issue;
   const no = context.issue.number;
+  const files = await github.rest.pulls.listFiles({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: no,
+  });
 
-  core.info("-----Issue-----");
-  core.info(JSON.stringify(issue, null, 2));
+  if (files.data.some((file) => file.filename === "info.yml")) {
+    const infoRaw = files.data.find((file) => file.filename === "info.yml")?.patch;
 
-  core.info("-----no-----");
-  core.info(no.toString());
-
-  if (!issue) return;
-
-  const labels: string[] = (issue.labels || []).map((i: any) => i && i.name).filter(Boolean);
-
-  if (labels.includes("new-challenge")) {
-    const locale = labels.includes("ja") ? "ja" : labels.includes("zh-CN") ? "zh-CN" : "en";
-    const body = issue.body || "";
-    const infoRaw = getCodeBlock(body, Messages[locale].info, "yaml");
     let info: any;
 
     try {
@@ -96,18 +58,11 @@ const action: Action = async (github, context, core) => {
 
     const dir = `challenges/${getQuestionFullName(no, info.difficulty, info.title)}`;
 
-    fs.writeFile(resolveFilePath(dir, "info", "yml", locale), `${YAML.dump(info)}\n`);
+    fs.writeFile(resolveFilePath(dir, "info", "yml", "en"), `${YAML.dump(info)}\n`);
   } else {
     core.info("No matched labels, skipped");
   }
 };
-
-function getCodeBlock(text: string, title: string, lang = "ts") {
-  const regex = new RegExp(`## ${title}[\\s\\S]*?\`\`\`${lang}([\\s\\S]*?)\`\`\``);
-  const match = text.match(regex);
-  if (match && match[1]) return match[1].toString().trim();
-  return null;
-}
 
 export function getQuestionFullName(no: number, difficulty: string, title: string) {
   return `${String(no).padStart(5, "0")}-${difficulty}-${slug(title.replace(/\./g, "-").replace(/<.*>/g, ""), {
