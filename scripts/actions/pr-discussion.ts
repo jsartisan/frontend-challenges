99baf654ba17ad36aaa091b640ac854507907413
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-05-16 08:56:08 +0530

    update workflow

--- scripts/actions/pr-discussion.ts
+++ scripts/actions/pr-discussion.ts
@@ -40,7 +40,7 @@ const action: Action = async (github, context, core) => {
       const { data: response } = await github.graphql<any>(
         `mutation {
             createDiscussion(input: {
-              repositoryId: "721944613",
+              repositoryId: 721944613,
               title: "${info.title}",
               categoryId: "general",
               body: "This is a discussion for #${no} - ${info.title}",
