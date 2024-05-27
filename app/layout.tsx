e6781ea203a22835ea0413e15be29c731882badf
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-05-27 22:26:29 +0530

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,10 @@ import { cn } from "@/utils/helpers";
 import { Provider } from "@/providers";
 import { getChallenges } from "@/db/challenge";
 import { Header } from "@/components/layout/Header";
+import { DOMAIN } from "@/constants";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(DOMAIN),
   title: "Frontend Challenges",
   description:
     "Frontend Challenges is a collection of frontend interview questions and answers. It is designed to help you prepare for frontend interviews. It's free and open source.",
