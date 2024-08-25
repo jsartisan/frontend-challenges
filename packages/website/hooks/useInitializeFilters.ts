415f0cab434e71089d048b645cb75d1e391e7d82
Pawan Kumar <pawan.stardust@gmail.com>
2024-08-25 19:15:04 +0530

    fix filters bug

--- packages/website/hooks/useInitializeFilters.ts
+++ packages/website/hooks/useInitializeFilters.ts
@@ -6,11 +6,9 @@ import { getSessionStorageItem } from "../utils/sessionStorage";
 
 export function useInitializeFilters(scope: string, filters: Partial<ChallengeFilterState>, dispatch: any) {
   useEffect(() => {
-    const category = getSessionStorageItem(`${STORAGE_KEY}:${scope}:category`, filters?.category || []);
-    const difficulty = getSessionStorageItem(`${STORAGE_KEY}:${scope}:difficulty`, filters?.difficulty || []);
-    const type = (sessionStorage.getItem(`${STORAGE_KEY}:${scope}:type`) ||
-      filters.type ||
-      "all") as ChallengeFilterState["type"];
+    const category = getSessionStorageItem(`${STORAGE_KEY}:${scope}:category`, []);
+    const difficulty = getSessionStorageItem(`${STORAGE_KEY}:${scope}:difficulty`, []);
+    const type = (sessionStorage.getItem(`${STORAGE_KEY}:${scope}:type`) || "all") as ChallengeFilterState["type"];
 
     dispatch({ category, difficulty, type });
   }, []);
